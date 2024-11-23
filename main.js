692ddb7de73019c75e124ad8cec3ffbef23ca4dc
peklos <160431596+peklos@users.noreply.github.com>
2024-11-24 00:10:31 +0300

    aaaaa

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 console.log('asdasda')
 var swiper = new Swiper(".mySwiper", {
       spaceBetween: 30,
-      loop: true,
       pagination: {
         el: ".swiper-pagination",
         clickable: true,
