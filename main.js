bac48aa35c61ad24cc3ee825f9b0917daf93c1ff
peklos <160431596+peklos@users.noreply.github.com>
2024-11-24 00:06:29 +0300

    asd

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 console.log('asdasda')
+var swiper = new Swiper(".mySwiper", {
+      spaceBetween: 30,
+      loop: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+
+    });
